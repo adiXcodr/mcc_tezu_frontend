7e93607e1b14162570ea7041bb44470240e7e068
shrawan <shrawansb2000@gmail.com>
2020-04-28 21:03:39 +0530

    removed mozilla logo

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,10 @@
 $(document).ready(function(){
     $('i').click(function(){
         $('.main_header').toggleClass('active');
-        $('.main_header').toggleClass('tray-transition');
     })
 })
 
-var windowWidth = $(window).width();
-if(windowWidth<768)
-{
-    document.getElementById('header_logo').classList.remove('mobil');
-    document.getElementById('header_logo').classList.add('show');
-}
+
+
+
 
